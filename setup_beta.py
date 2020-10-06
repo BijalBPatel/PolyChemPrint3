ca6d6cf538ea2fc92989585116a33687dd419bbb
Bijal Patel <bbpatel2@illinois.edu>
2020-10-06 12:43:57 -0500

    Beta PyPI

---
+++ setup_beta.py
@@ -0,0 +1,133 @@
+""" Adapted from sample setup module at links below
+A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+from os import path
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='polychemprint3-beta',  # Required
+    version='3.0.1b1',  # Required
+    description='FOSS additive manufacturing control software targeted at research Users (Polymer/Paste/BioPrinting) ',
+    long_description=long_description,  # Optional
+    long_description_content_type='text/x-rst',  # Optional (see note above)
+    url='https://github.com/BijalBPatel/PolyChemPrint3/tree/beta',  # Optional
+    author='Bijal Patel',  # Optional
+    author_email='bbpatel2@illinois.edu',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # These classifiers are *not* checked by 'pip install'. See instead
+        # 'python_requires' below.
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='3D Printing Additive Manufacturing',  # Optional
+    package_dir={'.': 'polychemprint3'},  # Optional
+	packages=find_packages(),  # Required
+	py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+	include_package_data=True,
+    python_requires='>=3.0',
+
+    install_requires=[
+		'pyserial', 
+		'colorama',
+		'pyyaml', 
+		'psutil', 
+		'babel', 
+		'pillow', 
+		'tkfilebrowser', 
+		],  # Optional
+
+ 
+    extras_require={
+    'win32': 'pywin32'  # Optional
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #
+    # If using Python 2.6 or earlier, then these have to be included in
+    # MANIFEST.in as well.
+    #package_data={  # Optional
+    #    'sample': ['package_data.dat'],
+    #},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'polychemprint3-beta = polychemprint3:main',
+        ],
+    },
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/BijalBPatel/PolyChemPrint3/issues',
+        'Documentation': 'https://polychemprint3.readthedocs.io',
+        'Source': 'https://github.com/BijalBPatel/PolyChemPrint3/tree/beta',
+    },
+)
